a0a92c6f395755285fc60a49f981be1360368a9f
pmCoach <65163988+pmCoach@users.noreply.github.com>
2021-02-08 22:59:17 +0400

    Update ClientService.java

--- src/main/java/com/haulmont/testtask/service/ClientService.java
+++ src/main/java/com/haulmont/testtask/service/ClientService.java
@@ -8,7 +8,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
 
-
+//Класс предназначен для взаимодействия с БД
 public class ClientService {
     private Factory factory = Factory.getInstance();
 
