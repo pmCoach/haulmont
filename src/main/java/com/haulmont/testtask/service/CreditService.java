ae2463a4cc75ca987b0833eaf39abe9144ad52fa
pmCoach <65163988+pmCoach@users.noreply.github.com>
2021-02-08 22:59:58 +0400

    Update CreditService.java

--- src/main/java/com/haulmont/testtask/service/CreditService.java
+++ src/main/java/com/haulmont/testtask/service/CreditService.java
@@ -8,6 +8,7 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
+//Класс предназначен для взаимодействия с БД
 public class CreditService {
     CreditDAO factory = Factory.getInstance().getCreditDAO();
 
